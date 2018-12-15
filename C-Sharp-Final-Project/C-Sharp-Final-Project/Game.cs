c29e2633070bc54a0e83251ba631e79eb3448ba9
tranqnhan <nt305748@gmail.com>
2018-12-15 15:43:49 -0600

    added pathfinder

--- C-Sharp-Final-Project/C-Sharp-Final-Project/Game.cs
+++ C-Sharp-Final-Project/C-Sharp-Final-Project/Game.cs
@@ -8,23 +8,32 @@ namespace C_Sharp_Final_Project
     {
         private bool isRunning;
         private IntPtr window;
-        public static IntPtr renderer;
+        private Level currentLevel;
+        public static IntPtr Renderer;
+        public static Grid Grid;
+        public static int Width;
+        public static int Height;
 
         public Game(){}
 
-        public void Init(string title, int xpos, int ypos, int width, int height)
+        public void Init(string title, int xPos, int yPos, int width, int height)
         {
             SDL_WindowFlags flags = 0;
+            Width = width;
+            Height = height;
             if (SDL_Init(SDL_INIT_EVERYTHING) == 0)
             {
-                window = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
-                renderer = SDL_CreateRenderer(window, -1, 0);
-                SDL_SetRenderDrawColor(renderer, 200, 200, 50, 90);
+                window = SDL_CreateWindow(title, xPos, yPos, width, height, flags);
+                Renderer = SDL_CreateRenderer(window, -1, 0);
+                SDL_SetRenderDrawColor(Renderer, 200, 200, 50, 90);
                 isRunning = true;
-            }
-
+            }  
         }
-
+        public void SetUpNextLevel(string levelFilePath) //Calls in Update
+        {
+            currentLevel = new Level(levelFilePath);
+            Grid = currentLevel.grid;
+        }
         public void HandleEvents()
         {
             SDL_Event events;
@@ -49,18 +58,18 @@ namespace C_Sharp_Final_Project
 
         public void Render()
         {
-            SDL_SetRenderDrawColor(renderer, 200, 200, 50, 90);
-            SDL_RenderClear(renderer);
+            SDL_SetRenderDrawColor(Renderer, 200, 200, 50, 90);
+            SDL_RenderClear(Renderer);
 
             //Render Objects
 
-            SDL_RenderPresent(renderer);
+            SDL_RenderPresent(Renderer);
         }
 
         public void Clean()
         {
             SDL_DestroyWindow(window);
-            SDL_DestroyRenderer(renderer);
+            SDL_DestroyRenderer(Renderer);
             SDL_Quit();
         }
 
